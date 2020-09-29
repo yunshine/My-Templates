293ce1afde7fb32cae4237e1d55318ee2c299577
Yun Chung <yun.chung@mac.com>
2020-09-29 15:47:03 +0900

    Added partials to template

--- My Express Template/index.js
+++ My Express Template/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+// app.set('view engine', 'ejs');
+
 //  ***** ROUTES *****
 
 // "/" => "Hi there!"  *** DEFAULT ROUTE ***
