488860ece3cccb83c07909df4e379e484ab6ca87
Yun Chung <yun.chung@mac.com>
2020-09-29 13:45:14 +0900

    Added a views folder to the Express template

--- My Express Template/index.js
+++ My Express Template/index.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var app = express();
 
-//  *****Routes*****
+//  ***** ROUTES *****
+
 // "/" => "Hi there!"  *** DEFAULT ROUTE ***
 app.get('/', function (req, res) {
   res.send('Hi there!');
 });
 
-// "/bye" => "Goodbye!"  *** Another Route ***
-app.get('/bye', function (req, res) {
-  console.log('SOMEONE MADE A REQUEST TO /BYE!!!');
-  res.send('Goodbye!!');
-});
-
-// "/dog" => "MEOW!"
+// "/dog" => "MEOW!"  *** Another Route ***
 app.get('/dog', function (req, res) {
   console.log('SOMEONE MADE A REQUEST TO /DOG!!!');
-  res.send('MEOW!');
+  res.send('<h2>MEOW!</h2>');
 });
 
 //  *** USING  :  FOR PARAMS ***
@@ -25,13 +20,25 @@ app.get('/r/:subredditName', function (req, res) {
   res.send('WELCOME TO THE ' + subreddit.toUpperCase() + ' SUBREDDIT!');
 });
 
+//  *** Render an HTML/EJS FILE... ***
+app.get('/home/:something/:id', function (req, res) {
+  let somethingVar = req.params.something;
+  let idVar = req.params.id;
+
+  res.send('home.ejs', {
+    somethingToPass: somethingVar,
+    idToPass: idVar,
+  });
+  res.send('<h2>A random message...</h2>');
+});
+
 //  *** USE A * AS A DEFAULT CATCH ALL ***
 app.get('*', function (req, res) {
   res.send('YOU ARE A STAR!!!');
 });
 
-// Tell Express to listen for requests (start server)
+// Tell Express to listen for requests (start server)...
 
 app.listen(process.env.PORT, process.env.IP, function () {
-  console.log('Server has started!!!');
+  console.log('The server has started...');
 });
