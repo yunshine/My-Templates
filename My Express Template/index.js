9ca03787160068b453cc593e0cec1f260d364732
Yun Chung <yun.chung@mac.com>
2020-09-29 17:12:48 +0900

    Edited Express template to work with body-parser

--- My Express Template/index.js
+++ My Express Template/index.js
@@ -3,25 +3,26 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}))
 // app.set('view engine', 'ejs');
 
 //  ***** ROUTES *****
 
 // "/" => "Hi there!"  *** DEFAULT ROUTE ***
 app.get('/', function (req, res) {
-  res.render('Hi there!');
+  res.send('Hi there!');
 });
 
 // "/dog" => "MEOW!"  *** Another Route ***
 app.get('/dog', function (req, res) {
   console.log('SOMEONE MADE A REQUEST TO /DOG!!!');
-  res.render('<h2>MEOW!</h2>');
+  res.send('<h2>MEOW!</h2>');
 });
 
 //  *** USING  :  FOR PARAMS ***
 app.get('/r/:subredditName', function (req, res) {
   var subreddit = req.params.subredditName;
-  res.render('WELCOME TO THE ' + subreddit.toUpperCase() + ' SUBREDDIT!');
+  res.send('WELCOME TO THE ' + subreddit.toUpperCase() + ' SUBREDDIT!');
 });
 
 //  *** Render an HTML/EJS FILE... ***
@@ -33,28 +34,32 @@ app.get('/home/:something/:id', function (req, res) {
     somethingToPass: somethingVar,
     idToPass: idVar,
   });
-  res.render('<h2>A random message...</h2>');
 });
 
-app.get('/friends', function (req, res) {
-  let friends = ["Woo", "Eugene", "Jason", "Cliff", "Eugene"];
 
-  res.render('friends.ejs', { friends: friends });
-});
+//   *** POST Route => see form on friends.ejs ***
+let friends= ["Woo", "Eugene", "Jason", "Cliff", "Eugene"];
 
 //   *** POST Route => see form on friends.ejs ***
 // BE SURE TO npm install body-parser --save
 app.post("/addfriend", function (req, res) {
-  res.send('');
+  // the "newfriend" in req.body.newfriend below comes from name="newfriend" from the <input>"
+  let newFriendName = req.body.newfriend;
+  friends.push(newFriendName);
+  res.redirect('friends.ejs');
 };
+
+app.get('/friends', function (req, res) {
+  res.render('friends.ejs', { friends: friends });
+});
 
 //  *** USE A * AS A DEFAULT CATCH ALL ***
 app.get('*', function (req, res) {
   res.render('YOU ARE A STAR!!!');
 });
 
-// Tell Express to listen for requests (start server)...
 
+// Tell Express to listen for requests (start server)...
 app.listen(process.env.PORT, process.env.IP, function () {
   console.log('The server has started...');
 });
